51fc3072c7f69e88e329017fe1afb09d4c999ba8
Renato Trevisiol Montiel <renatotrevisiolmontiel@gmail.com>
2025-04-13 12:33:55 -0300

    Ejercicio 8 y 10 finalizados

---
+++ js/ejercicio10.js
@@ -0,0 +1,7 @@
+let num1= parseInt(prompt("Elegir número 1"));
+let num2= parseInt(prompt("Elegir número 2"));
+let num3= parseInt(prompt("Elegir número 3"));
+
+let promedio= (num1+num2+num3)/3;
+
+alert("El promedio de los tres números es: " + promedio.toFixed(2));
---
+++ js/ejercicio8.js
@@ -0,0 +1,9 @@
+function calcularMayor(numero1,numero2){
+
+  if(numero1==numero2){
+    alert("Los números son iguales");
+  }else if(numero1>numero2){
+    alert(numero1 + " es mayor a " + numero2);
+  }else
+    alert(numero2 + " es mayor a " + numero1);
+}
