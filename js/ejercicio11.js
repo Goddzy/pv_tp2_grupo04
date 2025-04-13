88993144f75939a1592190d5a235e0b97cee374e
benjamin burgos <benjaopuy@gmail.com>
2025-04-13 15:04:06 -0300

    Ejercicios 11 y 12

---
+++ js/ejercicio11.js
@@ -0,0 +1,6 @@
+let KM = parseFloat(prompt("Ingrese la cantidad de kilómetros recorridos:"));
+let LITROS = parseFloat(prompt("Ingrese la cantidad de litros consumidos:"));
+
+let consumo = LITROS / KM;
+
+alert("El consumo de combustible es: " + consumo);
---
+++ js/ejercicio12.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt("Ingrese un número:"));
+
+if (num1 % 2 == 0) {
+    alert("El número es par.");
+}
+else {
+    alert("El número es impar.");
+}
