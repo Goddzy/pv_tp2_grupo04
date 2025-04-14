7950b84749f45e4f0231b1110ab107f8c3dd49d2
GUSTAVO CARI <gustavo104846@gmail.com>
2025-04-14 01:41:42 -0300

    ejercicio 13 y 14 resuelto por Cari Enrique Gustavo

--- js/ejercicio12.js
+++
@@ -1,7 +0,0 @@
-let num1= parseInt(prompt("Ingrese un numero "));
-
-if (num1 % 2 === 0) {
-    alert("El número : "+ num1 +" es par");
-} else {
-    alert("El número : "+ num1 + " es impar");
-}
---
+++ js/ejercicio13.js
@@ -0,0 +1,9 @@
+const mostrarArreglo = (numeros) => {
+    for (let i = 0; i < numeros.length; i++) {
+        console.log(numeros[i]);
+    }
+};
+
+
+let numeros = [1, 2, 3, 4, 5];
+mostrarArreglo(numeros); 
